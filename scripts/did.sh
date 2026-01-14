b1ecec9b607d53ed77cbb13165da94b21b118f85
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-14 21:47:38 +0100

    fix: Correct script for generating DID files

--- scripts/did.sh
+++ scripts/did.sh
@@ -2,15 +2,18 @@
 
 function generate_did() {
   local canister=$1
+
+  local crate_name="${canister//-/_}"
+
   canister_root="src/$canister"
 
-  test -e "target/wasm32-unknown-unknown/release/$canister.wasm" ||
+  test -e "target/wasm32-unknown-unknown/release/$crate_name.wasm" ||
     cargo build --manifest-path="$canister_root/Cargo.toml" \
       --target wasm32-unknown-unknown \
       --release --package "$canister"
 
   # cargo install candid-extractor
-  candid-extractor "target/wasm32-unknown-unknown/release/$canister.wasm" >"$canister_root/$canister.did"
+  candid-extractor "target/wasm32-unknown-unknown/release/$crate_name.wasm" >"$canister_root/$canister.did"
 }
 
 generate_did icrc-factory
