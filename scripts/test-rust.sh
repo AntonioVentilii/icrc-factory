b1c64355b7fd7f67169579a6b90196028687141d
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-01-17 22:00:54 +0100

    correct tests

--- scripts/test-rust.sh
+++ scripts/test-rust.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-RUST_BACKTRACE=1 RUSTFLAGS="-D warnings" cargo test --lib --bins --benches --examples --all-features
+RUST_BACKTRACE=1 RUSTFLAGS="-D warnings" cargo test --all-features
